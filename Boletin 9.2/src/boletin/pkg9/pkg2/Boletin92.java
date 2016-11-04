8b07bd80c98158ce03d47bfaf06adefa86a1146c
Diego <Diego@Diego>
2016-11-04 12:19:38 +0100

    9.2 salida por consola.

---
+++ Boletin 9.2/src/boletin/pkg9/pkg2/Boletin92.java
@@ -0,0 +1,13 @@
+package boletin.pkg9.pkg2;
+// @author Diego
+public class Boletin92 {
+
+    public static void main(String[] args) {
+        
+        Metodo obj= new Metodo();
+        
+        obj.calculos();
+        
+    }
+    
+}
---
+++ Boletin 9.2/src/boletin/pkg9/pkg2/Metodo.java
@@ -0,0 +1,20 @@
+package boletin.pkg9.pkg2;
+
+public class Metodo {
+    
+    public void calculos(){
+        
+        int n1;
+        int n2;
+        
+        for(n1=10; n1<=90; n1++){
+            n2 = 10;
+            do{
+                System.out.println(n1 + " + " + n2 + " = " + (n1+n2) + " & " + n1 + " * " + n2 + " = " + (n1*n2));
+                n2++;
+            }while(n2<=90);
+            n1++;
+        }
+    }
+    
+}
