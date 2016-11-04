91744f087899666b7eb73d55d37142d2beda35d5
Diego <Diego@Diego>
2016-11-04 12:41:11 +0100

    9.3

---
+++ Boletin 9.3/src/boletin/pkg9/pkg3/Boletin93.java
@@ -0,0 +1,12 @@
+package boletin.pkg9.pkg3;
+// @author Diego
+public class Boletin93 {
+
+    public static void main(String[] args) {
+        
+        Metodos obj = new Metodos();
+        
+        obj.calculos();
+    }
+    
+}
---
+++ Boletin 9.3/src/boletin/pkg9/pkg3/Metodos.java
@@ -0,0 +1,29 @@
+package boletin.pkg9.pkg3;
+
+import javax.swing.JOptionPane;
+
+public class Metodos {
+    private double base, altura;
+    
+    public Metodos(){    
+    }
+    
+    public Metodos(double b, double h){
+        base = b;
+        altura = h;
+    }
+    
+    public void calculos(){
+        
+        base = Double.parseDouble(JOptionPane.showInputDialog("Introduce la base."));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Introduce la altura."));
+        
+        do{
+            base = Double.parseDouble(JOptionPane.showInputDialog("Introduce la base otra vez.\nEsta vez mayor que cero."));
+            altura = Double.parseDouble(JOptionPane.showInputDialog("Introduce la alturaotra vez.\nEsta vez mayor que cero."));
+        }while(base<=0|altura<=0);
+        
+        JOptionPane.showMessageDialog(null, "El area del rectangulo de base " + base + "cm y altura " + altura + "cm es igual a " + base*altura + "cm^2");
+    }
+    
+}
